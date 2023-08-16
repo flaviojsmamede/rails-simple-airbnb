28ae6f1f90d3d5e70c2e06a389a5ec6a127ede1c
Flavio Mamede <flaviojsmamede@gmail.com>
2023-08-16 17:10:26 +0100

    exercise 2 incomplete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+puts 'Creating flats...'
+
+4.times do
+  Flat.create(
+    name: Faker::Company.catch_phrase,
+    address: Faker::Address.street_address,
+    description: Faker::Quote.famous_last_words,
+    price_per_night: (0..300).to_a.sample,
+    number_of_guests: (0..10).to_a.sample
+  )
+end
+
+puts 'Finished!'
